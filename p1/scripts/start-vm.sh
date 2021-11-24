585502cea8c9f3f187a9535255672dd4a38eedb8
Fedor-Molokov <horosho999@gmail.com>
2021-11-24 22:46:13 +0300

    delete all comments in p1

--- p1/scripts/start-vm.sh
+++ p1/scripts/start-vm.sh
@@ -3,5 +3,4 @@ cd ../.
 sudo vagrant up
 cd -
 ansible-galaxy install -r ./ansible/requirements.yaml
-# ansible-playbook -i ./ansible/inventory/inventory.yaml ./ansible/all.yaml
 ansible-playbook -i ./ansible/inventory/host.ini ./ansible/all.yaml
